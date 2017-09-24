68380b440c59843663b666dd6a7c73ca96a56d5e
Anurag Anand <anurag21j@gmail.com>
2017-09-24 08:01:27 +0530

    User Forgot Password ok

--- src/servlets/ForgotPassword.java
+++ src/servlets/ForgotPassword.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import bean.UserBean;
+import dao.UserImpl;
 import mails.Mailer;
 
 /**
@@ -24,10 +26,19 @@ public class ForgotPassword extends HttpServlet {
 		PrintWriter out = response.getWriter();
 		out.println("Welcome");
 		try {
-			// from,password,to,subject,message
-			Mailer mail = new Mailer();
-			mail.send("javammu@gmail.com", "dellintel", "anurag21j@gmail.com", "Women Harrasmant", id);
-			out.println("Sent");
+			UserImpl dao = new UserImpl();
+			UserBean bean = dao.findByID(id);
+			if(bean != null)
+			{
+				String s = "Dear "+bean.getName()+"\n"+"Your password Reset Request:\n\t"+"Id: "+id+"\nPassword: "+bean.getPassword();
+				Mailer mail = new Mailer();
+				mail.send("javammu@gmail.com", "dellintel", bean.getEmail(), "Reset Password", s);
+				out.println("Sent");
+			}
+			else {
+				out.println("Invalid User ID \n Do you have a account? Register Now");
+			}
+			
 
 		} catch (RuntimeException e) {
 			out.println("Error");
